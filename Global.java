7c565904bacedb6b8280d64c754fe396530d8347
radhikak1999 <radhikak8123@gmail.com>
2023-06-27 08:49:04 +0530

    first commit

---
+++ Global.java
@@ -0,0 +1,10 @@
+class Global {
+    static int a=10;
+	a=20;
+    public static void main(String[] args) {
+      //Sample s=new Sample();
+    //   int a=30;
+        System.out.println(a);
+        
+    }
+}
